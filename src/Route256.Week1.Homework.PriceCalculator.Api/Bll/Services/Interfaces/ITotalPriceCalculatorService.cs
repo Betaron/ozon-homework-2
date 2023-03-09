8dc63934eeef0fd04f5a4b389d0d0784cff162bc
Иван Агабалаев <agabalaev01@yandex.ru>
2023-03-09 03:58:58 +0300

    Add a service for calculating the total cost of goods

---
+++ src/Route256.Week1.Homework.PriceCalculator.Api/Bll/Services/Interfaces/ITotalPriceCalculatorService.cs
@@ -0,0 +1,6 @@
+﻿namespace Route256.Week1.Homework.PriceCalculator.Api.Bll.Services.Interfaces;
+
+public interface ITotalPriceCalculatorService
+{
+	decimal CalculateTotalPrice(int goodId, int distance);
+}
---
+++ src/Route256.Week1.Homework.PriceCalculator.Api/Bll/Services/TotalPriceCalculatorService.cs
@@ -0,0 +1,34 @@
+﻿using Route256.Week1.Homework.PriceCalculator.Api.Bll.Models.PriceCalculator;
+using Route256.Week1.Homework.PriceCalculator.Api.Bll.Services.Interfaces;
+using Route256.Week1.Homework.PriceCalculator.Api.Dal.Repositories.Interfaces;
+
+namespace Route256.Week1.Homework.PriceCalculator.Api.Bll.Services;
+
+public class TotalPriceCalculatorService : ITotalPriceCalculatorService
+{
+	private readonly IGoodsRepository _goodsRepository;
+	private readonly IPriceCalculatorService _priceCalculatorService;
+
+	public TotalPriceCalculatorService(
+		IGoodsRepository goodsRepository,
+		IPriceCalculatorService priceCalculatorService)
+	{
+		_goodsRepository = goodsRepository;
+		_priceCalculatorService = priceCalculatorService;
+	}
+
+	public decimal CalculateTotalPrice(int goodId, int distance)
+	{
+		var good = _goodsRepository.Get(goodId);
+		var model = new GoodModel(
+			good.Height,
+			good.Length,
+			good.Width,
+			good.Weight);
+
+		var deliveryPrice =
+			_priceCalculatorService.CalculatePrice(new[] { model }, distance);
+
+		return deliveryPrice + good.Price;
+	}
+}
