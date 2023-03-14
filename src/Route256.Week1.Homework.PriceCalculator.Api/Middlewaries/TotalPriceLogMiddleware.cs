a1497e0bc20b6a9a4e77fe2cc564504da93045ed
Иван Агабалаев <agabalaev01@yandex.ru>
2023-03-14 15:27:09 +0300

    Review - Use using instead of manually closing the stream
    Extract a method for non-destructive reading of any stream.

--- src/Route256.Week1.Homework.PriceCalculator.Api/Middlewaries/TotalPriceLogMiddleware.cs
+++ src/Route256.Week1.Homework.PriceCalculator.Api/Middlewaries/TotalPriceLogMiddleware.cs
@@ -21,10 +21,7 @@ internal sealed class TotalPriceLogMiddleware
 		if (context.Request.Path.StartsWithSegments("/v1/V1OrderPrice/calculate-total")
 			&& context.Request.Method == "POST")
 		{
-			context.Request.Body.Position = 0;
-			var requestReader = new StreamReader(context.Request.Body);
-			var requestBodyContent = await requestReader.ReadToEndAsync();
-			context.Request.Body.Position = 0;
+			var requestBodyContent = await ReadStreamAsync(context.Request.Body);
 
 			var requestInfo = new
 			{
@@ -40,17 +37,9 @@ internal sealed class TotalPriceLogMiddleware
 
 			await _next.Invoke(context);
 
-			requestReader.Close();
-
-			responseStream.Position = 0;
-			var responseReader = new StreamReader(responseStream);
-			var responceBodyContent = await responseReader.ReadToEndAsync();
-
-			responseStream.Position = 0;
+			var responceBodyContent = await ReadStreamAsync(responseStream);
 			context.Response.Body = originalResponseBodyStream;
 			await responseStream.CopyToAsync(context.Response.Body);
-
-			responseReader.Close();
 
 			var logMessage = new StringBuilder();
 
@@ -59,7 +48,9 @@ internal sealed class TotalPriceLogMiddleware
 			logMessage.AppendLine($"\tUrl: {requestInfo.Url}");
 			logMessage.AppendLine($"\tHeaders:");
 			foreach (var header in requestInfo.Headers)
+			{
 				logMessage.AppendLine($"\t\t{header.Key}: {header.Value}");
+			}
 			logMessage.AppendLine($"\tBody: {requestInfo.Body}");
 			logMessage.AppendLine($"Response:");
 			logMessage.AppendLine($"\tBody: {responceBodyContent}");
@@ -70,5 +61,17 @@ internal sealed class TotalPriceLogMiddleware
 		{
 			await _next.Invoke(context);
 		}
+	}
+
+	private async Task<string> ReadStreamAsync(Stream targetStream)
+	{
+		var streamContent = string.Empty;
+		using (var sr = new StreamReader(
+			targetStream, Encoding.UTF8, leaveOpen: true))
+		{
+			streamContent = await sr.ReadToEndAsync();
+		}
+
+		return streamContent;
 	}
 }
