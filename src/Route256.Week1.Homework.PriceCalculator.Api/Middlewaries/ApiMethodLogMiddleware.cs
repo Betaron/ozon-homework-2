b072a3291c92f512890b275d9b7bd0b7b831d57b
Иван Агабалаев <agabalaev01@yandex.ru>
2023-03-16 00:51:32 +0300

    Refactor Log middleware

--- src/Route256.Week1.Homework.PriceCalculator.Api/Middlewaries/ApiMethodLogMiddleware.cs
+++ src/Route256.Week1.Homework.PriceCalculator.Api/Middlewaries/ApiMethodLogMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Primitives;
 using Route256.Week1.Homework.PriceCalculator.Api.Attributes;
 
 namespace Route256.Week1.Homework.PriceCalculator.Api.Middlewaries;
@@ -25,41 +26,19 @@ internal sealed class ApiMethodLogMiddleware
 
 		if (attribute != null)
 		{
-			var requestBodyContent = await ReadStreamAsync(context.Request.Body);
-
-			var requestInfo = new
-			{
-				Timestamp = DateTime.UtcNow.ToString(),
-				Url = context.Request.GetDisplayUrl(),
-				Headers = context.Request.Headers,
-				Body = requestBodyContent
-			};
+			await using var responceStream = new MemoryStream();
 
 			var originalResponseBodyStream = context.Response.Body;
-			await using var responseStream = new MemoryStream();
-			context.Response.Body = responseStream;
+			context.Response.Body = responceStream;
 
 			await _next.Invoke(context);
 
-			var responceBodyContent = await ReadStreamAsync(responseStream);
+			await Log(context.Request, context.Response);
+
+			responceStream.Position = 0;
+
+			await responceStream.CopyToAsync(originalResponseBodyStream);
 			context.Response.Body = originalResponseBodyStream;
-			await responseStream.CopyToAsync(context.Response.Body);
-
-			var logMessage = new StringBuilder();
-
-			logMessage.AppendLine($"{Environment.NewLine}Request: ");
-			logMessage.AppendLine($"\tTimestamp: {requestInfo.Timestamp}");
-			logMessage.AppendLine($"\tUrl: {requestInfo.Url}");
-			logMessage.AppendLine($"\tHeaders:");
-			foreach (var header in requestInfo.Headers)
-			{
-				logMessage.AppendLine($"\t\t{header.Key}: {header.Value}");
-			}
-			logMessage.AppendLine($"\tBody: {requestInfo.Body}");
-			logMessage.AppendLine($"Response:");
-			logMessage.AppendLine($"\tBody: {responceBodyContent}");
-
-			_logger.LogInformation(logMessage.ToString());
 		}
 		else
 		{
@@ -67,15 +46,58 @@ internal sealed class ApiMethodLogMiddleware
 		}
 	}
 
-	private async Task<string> ReadStreamAsync(Stream targetStream)
+	private static async Task<string> ReadStreamAsync(Stream stream)
 	{
-		var streamContent = string.Empty;
-		using (var sr = new StreamReader(
-			targetStream, Encoding.UTF8, leaveOpen: true))
-		{
-			streamContent = await sr.ReadToEndAsync();
-		}
+		var initialPosition = stream.Position;
 
-		return streamContent;
+		stream.Position = 0;
+		using var sr = new StreamReader(stream, Encoding.UTF8, leaveOpen: true);
+		var content = await sr.ReadToEndAsync();
+
+		stream.Position = initialPosition;
+
+		return content;
 	}
+
+	private async Task Log(HttpRequest request, HttpResponse response)
+	{
+		var requestBodyContent = await ReadStreamAsync(request.Body);
+		var responseBodyContent = await ReadStreamAsync(response.Body);
+
+		var logModel = new LogModel(
+			DateTime.UtcNow,
+			request.GetDisplayUrl(),
+			request.Headers,
+			requestBodyContent,
+			responseBodyContent);
+
+		Log(logModel);
+	}
+
+	private void Log(LogModel model)
+	{
+		var logMessage = new StringBuilder();
+
+		logMessage.AppendLine($"{Environment.NewLine}Request: ");
+		logMessage.AppendLine($"\tTimestamp: {model.Timestamp}");
+		logMessage.AppendLine($"\tUrl: {model.Url}");
+		logMessage.AppendLine($"\tHeaders:");
+		foreach (var header in model.Headers)
+		{
+			logMessage.AppendLine($"\t\t{header.Key}: {header.Value}");
+		}
+		logMessage.AppendLine($"\tBody: {model.RequestBody}");
+		logMessage.AppendLine($"Response:");
+		logMessage.AppendLine($"\tBody: {model.ResponseBody}");
+
+		_logger.LogInformation(logMessage.ToString());
+	}
+
+	private sealed record LogModel(
+		DateTime Timestamp,
+		string Url,
+		IDictionary<string, StringValues> Headers,
+		string RequestBody,
+		string ResponseBody
+	);
 }
